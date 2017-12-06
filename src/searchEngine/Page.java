d1d028fd732e8a0bfb5df6185defb997a15535db
Loke Carlsson <lc222ak@student.lnu.se>
2017-12-06 11:48:49 +0100

    stuff

---
+++ src/searchEngine/Page.java
@@ -0,0 +1,12 @@
+package searchEngine;
+
+import java.util.ArrayList;
+
+class Page {
+    String url;
+    ArrayList words;
+    Page() {
+        url = "";
+        words = new ArrayList<Integer>();
+    }
+}
